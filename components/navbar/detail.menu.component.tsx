3034b2dc8048569692521803fde306c09dece4f4
l.pathompong <pathompong.litk@gmail.com>
2026-05-26 13:04:00 +0700

    fix (navbar) : change style menu in navbar

--- components/navbar/detail.menu.component.tsx
+++ components/navbar/detail.menu.component.tsx
@@ -6,7 +6,7 @@ type prop = {
 
 export default async function Detail({ children }: prop) {
     return (
-        <div className="peer-checked/category:max-h-0 max-h-[500px]  ease-in-out flex flex-col *:p-2 *:px-8  overflow-hidden duration-700 ">
+        <div className="peer-checked/category:max-h-0 max-h-[500px] lg:hidden  ease-in-out flex flex-col *:p-2 *:px-8  overflow-hidden duration-700 ">
             {children}
         </div>
     )
