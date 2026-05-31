d6e23acaf4fc06b3afdb9800cb31b4b625ebf86a
l.pathompong <pathompong.litk@gmail.com>
2026-06-01 03:02:37 +0700

    change (loading) : change loading style

--- components/index.ts
+++ components/index.ts
@@ -2,12 +2,14 @@ import About from "./about/about.component";
 import Blog from "./blogs/blog.component";
 import Category from "./category/category.component";
 import LinkToUs from "./link-to-us/link.to.us.component";
+import LoadingComponent from "./loading/loading.component";
 import Navbar from "./navbar/navbar.component";
 import NavigationBar from "./navbar/navigation.component";
 import Product from "./product/product.component";
 import SearchProduct from "./search/search.component";
 
 export {
+  LoadingComponent,
   Blog,
   Navbar,
   SearchProduct,
---
+++ components/loading/loading.component.tsx
@@ -0,0 +1,10 @@
+export default function LoadingComponent() {
+    return (
+        <div className=" fixed w-full h-full bg-white text-4xl text-black z-99 flex justify-center items-center ">
+            <h1 className="animate-bounce">
+                Loading . . .
+            </h1>
+        </div>
+    )
+
+};
