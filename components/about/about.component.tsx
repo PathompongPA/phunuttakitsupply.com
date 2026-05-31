f7f10512c4abd696ff3a6427efd5156f2f4f0b5a
l.pathompong <pathompong.litk@gmail.com>
2026-06-01 01:18:08 +0700

    fix (about com) : change position background.

--- components/about/about.component.tsx
+++ components/about/about.component.tsx
@@ -7,10 +7,10 @@ export default function About() {
         <div className=" relative overflow-hidden w-full flex justify-center ">
             <div className=" grid grid-cols-1 md:grid-cols-2 gap-8 max-w-7xl md:gap-4 lg:gap-8 p-4 lg:p-16 ">
                 <Image className=" rounded-2xl z-10 " src={image} alt="" />
-                <div className=" flex flex-col justify-center items-end gap-8 z-10 border *:border">
+                <div className=" flex flex-col justify-center items-end gap-8 z-10 ">
                     <h1 className=" text-4xl font-bold text-wrap">ขับเคลื่อนทุกโรงงานไปกับอะไหล่ที่พร้อมใช้งาน</h1>
                     <p className=" text-gray-3 text-wrap text-[20px]">
-                        <h2 className=" text-orange w-full font-bold "> ภูณัฐกิจ ซัพพลายส์ จำกัด </h2>
+                        <span className=" text-orange w-full font-bold "> ภูณัฐกิจ ซัพพลายส์ จำกัด </span>
                         <span className="text-[16px]">
                             มุ่งมั่นสู่การเป็นผู้นำด้านการจัดจำหน่ายอะไหล่อุตสาหกรรมชิ้นส่วนเครื่องจักรและอุปกรณ์สำหรับโรงงานอุตสาหกรรม
                         </span>
@@ -18,8 +18,8 @@ export default function About() {
                     <Button>ข้อมูลเพิ่มเติม</Button>
                 </div>
             </div>
-            <span className=" absolute -left-1/5 -top-1/2 h-1/2 w-full  -rotate-20  bg-orange  z-0 "></span>
-            <span className=" absolute -right-1/5  -bottom-1/2 h-1/2 w-full  -rotate-20 bg-orange z-0 "></span>
+            <span className=" absolute -left-1/3 -top-1/2 h-1/2 w-[120%]  -rotate-20  bg-orange  z-0 "></span>
+            <span className=" absolute -right-1/3  -bottom-1/2 h-1/2 w-[120%]  -rotate-20 bg-orange z-0 "></span>
         </div>
     )
 };
