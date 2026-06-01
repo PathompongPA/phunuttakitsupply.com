d6935a5504f101fcfeeb66ac99b23b5bad1042ee
l.pathompong <pathompong.litk@gmail.com>
2026-06-02 03:18:10 +0700

    style (blog card) : change font size blog card

--- components/blogs/blog.card.component.tsx
+++ components/blogs/blog.card.component.tsx
@@ -13,9 +13,9 @@ export default function BlogCard({ id, title, subtitle, img }: prop) {
     const pathname = "/blogs/" + id
     return (
         <Link className="relative border-b-4 border-orange aspect-square min-w-62.5 lg:w-full rounded-t-lg overflow-hidden" href={{ pathname }}>
-            <div className="absolute bottom-0 left-1/2 gap-4 -translate-x-1/2 w-full p-4  z-10 text-white truncate flex flex-col *:w-full text-end">
-                <span className=" truncate text-[20px] font-bold" >{title}</span>
-                <span className=" truncate text-base font-semibold text-gray-3">{subtitle}</span>
+            <div className="absolute bottom-0 left-1/2 gap-2  -translate-x-1/2 w-full p-4  z-10 text-white truncate flex flex-col *:w-full text-end">
+                <span className=" truncate text-[16px] font-semibold" >{title}</span>
+                <span className=" text-wrap text-[12px] max-h-[50px] overflow-hidden  text-gray-3">{subtitle}</span>
             </div>
             <Image className="w-full h-full object-cover aspect-square brightness-45 " src={urlImage} width={0} height={0} alt="" unoptimized />
         </Link>
