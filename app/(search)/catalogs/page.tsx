244cff74effd2cf4549aecd87c2b59652a16e7fb
l.pathompong <pathompong.litk@gmail.com>
2026-06-05 00:25:56 +0700

    fea : download file catalog pdf

--- app/(search)/catalogs/page.tsx
+++ app/(search)/catalogs/page.tsx
@@ -16,7 +16,7 @@ export default async function Page({ searchParams }: Props) {
             <div className=" max-w-7xl w-full p-4 lg:p-8 lg:px-16 flex flex-col gap-4 min-h-screen">
                 <Title >Catalog</Title>
                 <div className=" grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-2 lg:gap-4 h-full">
-                    {catalog.length > 0 ? catalog?.map(({ id, name, thumbnail }) => <CatalogCard key={id} id={id} name={name} thumbnail={thumbnail} />) : <NotFoundItem />}
+                    {catalog.length > 0 ? catalog?.map(({ id, name, thumbnail, file }) => file && <CatalogCard key={id} file={file} id={id} name={name} thumbnail={thumbnail} />) : <NotFoundItem />}
                 </div>
             </div>
         </div>
--- components/catalog/catalog.card.component.tsx
+++ components/catalog/catalog.card.component.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image"
-import Link from "next/link"
 
 type prop = {
     id?: string,
     name?: string,
     thumbnail?: string,
+    file: string
 }
-export default function CatalogCard({ id, name, thumbnail }: prop) {
+export default function CatalogCard({ id, name, thumbnail, file }: prop) {
     const urlImage = process.env.NEXT_PUBLIC_URL_HOST + "assets/" + thumbnail
-    const pathname = "/catalogs/" + id
+    const urlFile = process.env.NEXT_PUBLIC_URL_HOST + "assets/" + file
     return (
-        <Link className=" p-1 lg:p-2 flex flex-col gap-2 w-full border-gray-2 border-2  aspect-3/4 rounded-lg" href={{ pathname }}>
+        <a className=" p-1 lg:p-2 flex flex-col gap-2 w-full border-gray-2 border-2  aspect-3/4 rounded-lg" href={urlFile} download target="_blank" >
             <Image className=" w-full h-full object-contain border-transparent" src={urlImage} alt="" width={0} height={0} unoptimized priority />
             <div className=" w-full text-center text-gray-4 text-[12px]">{name}</div>
-        </Link>
+        </a>
     )
 
 };
