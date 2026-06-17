5a5f97a7960a5562b6c9ba1b92774968aa0438a3
l.pathompong <pathompong.litk@gmail.com>
2026-06-18 05:00:00 +0700

    style (card product) : style animation on hover and active

--- components/product/product.card.component.tsx
+++ components/product/product.card.component.tsx
@@ -15,11 +15,11 @@ export default function ProductCard({ name, thumbnail, brand, category, type, br
     const urlThumbnail = process.env.NEXT_PUBLIC_URL_HOST_CLIENT + "assets/" + thumbnail
     const urlBrand = process.env.NEXT_PUBLIC_URL_HOST_CLIENT + "assets/" + brandImage
     return (
-        <Link href={{ pathname }} className="flex flex-col justify-between border border-gray-2 rounded-lg relative  ">
-            <div className=" absolute top-0 right-2 bg-white border aspect-square h-10 border-gray-2 border-t-0 rounded-b-lg">
+        <Link href={{ pathname }} className="flex flex-col justify-between border border-gray-2 rounded-lg relative active:scale-90 duration-200  ease-in-out lg:hover:shadow-xl/50 shadow-gray-3/50 lg:hover:border-3 overflow-hidden group  ">
+            <div className=" absolute top-0 right-2 bg-white border aspect-square h-10 border-gray-2 border-t-0 rounded-b-lg z-30 ">
                 <Image className="w-full h-full object-contain" src={urlBrand} alt="" width={0} height={0} unoptimized />
             </div>
-            <Image className=" aspect-square object-cover w-full" src={urlThumbnail} alt="" width={0} height={0} unoptimized />
+            <Image className=" group-hover:scale-110 z-0 duration-300 aspect-square object-cover w-full" src={urlThumbnail} alt="" width={0} height={0} unoptimized />
             <div className=" flex flex-col gap-1 p-2 ">
                 <div className="font-semibold text-[10px] lg:text-[10px] text-gray-3">{brand}</div>
                 <span className="font-semibold text-[12px] text-gray-5">{name}</span>
