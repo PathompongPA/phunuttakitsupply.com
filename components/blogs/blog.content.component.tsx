208e612161947835ca37c56c19d6bc88e87112a7
l.pathompong <pathompong.litk@gmail.com>
2026-06-17 11:29:07 +0700

    refactor (blog page) : re font size to bigger

--- components/blogs/blog.content.component.tsx
+++ components/blogs/blog.content.component.tsx
@@ -19,10 +19,10 @@ export default async function BlogContent({ id }: prop) {
     const createAt = dayjs(blog?.data_create).format("MMMM YYYY")
     return (
         <div className=" w-screen min-h-screen  flex flex-col items-center ">
-            <div className=" max-w-5xl min-h-screen bg-gry-1 flex flex-col gap-8 w-full md:px-16 ">
-                <div className=" flex flex-col gap-4 lg:gap-8 p-4 lg:p-16 items-center bg-gray-50 ">
-                    <div className=" w-full  text-[12px] md:text-[8px] lg:text-[12px] " dangerouslySetInnerHTML={{ __html: blog?.content }}></div>
-                    <div className=" flex gap-2 justify-end w-full text-[12px]">
+            <div className=" max-w-5xl min-h-screen bg-gry-1 flex flex-col gap-8 w-full md:px-4 ">
+                <div className=" flex flex-col gap-4 lg:gap-8 p-8 lg:p-8 items-center bg-gray-50 ">
+                    <div className=" w-full  text-[14px] md:text-[12px] lg:text-[16px] " dangerouslySetInnerHTML={{ __html: blog?.content }}></div>
+                    <div className=" flex gap-2 justify-end w-full text-[12px] ">
                         <div className=" text-gray-5">{createAt}</div>
                         <div className=" text-gray-5"> Posted by {blog?.user_updated?.first_name}</div>
                     </div>
