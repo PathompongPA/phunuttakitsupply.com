58e4cd4f317303b7ddc57c2d4898e241ed1165c1
l.pathompong <pathompong.litk@gmail.com>
2026-07-04 14:18:08 +0700

    style (auto slide) : change positon counting 20vh to 12%

--- components/slide/auto.slide.component.tsx
+++ components/slide/auto.slide.component.tsx
@@ -27,10 +27,10 @@ export default function AutoSlide({ children, interval = 8000 }: prop) {
     })
 
     return (
-        <div className="flex justify-center w-full   lg:pt-0 h-fit md:h-fit md:min-h-[50vh] md:items-center  lg:h-screen  relative overflow-hidden lg:max-h-192 "
+        <div className="flex justify-center w-full   lg:pt-0 h-fit md:h-fit md:min-h-[50vh] md:items-center  lg:h-screen  relative overflow-hidden lg:max-h-192"
         >
             {items[counterNumber]}
-            <div className=" flex flex-row gap-2 justify-center absolute bottom-[10vh] md:bottom-[20vh]  left-1/2 -translate-x-1/2 z-30 " >
+            <div className=" flex flex-row gap-2 justify-center absolute bottom-[12%]  left-1/2 -translate-x-1/2 z-30 " >
                 {count}
             </div >
         </div >
