eebabbce030b959430c0166d062c838942f82128
l.pathompong <pathompong.litk@gmail.com>
2026-07-09 20:09:26 +0700

    style (qr footer) : change bg color

--- components/footer/qr.contact.component.tsx
+++ components/footer/qr.contact.component.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image"
-import imageQrLine from "../../public/image/line-qr-v3.svg"
 import Link from "next/link"
+import { QRCodeSVG } from "qrcode.react"
 
 export default function QrContact() {
+    const urlLine = "https://line.me/ti/p/NuDh2mAbCf"
     return (
-        <div className=" text-orange text-center flex flex-col gap-2 font-bold  w-full md:w-fit justify-center items-center lg:py-8 rounded-3xl aspect-3/4 border-2 border-orange/20 bg-orange/5 ">
-            <Image className=" w-full  max-w-50 lg:max-w-45" src={imageQrLine} alt="qr-line" width={0} height={0} unoptimized />
-            <Link className="  text-orange text-lg  md:text-[16px] lg:text-[15px] " target="_blank" href={{ pathname: "https://line.me/ti/p/NuDh2mAbCf" }} >
+        <div className=" text-orange text-center flex flex-col gap-2 font-bold  w-full md:w-fit justify-center items-center lg:py-8 rounded-3xl aspect-3/4 bg-white ">
+            <QRCodeSVG value={urlLine} fgColor="#2A2A2A" bgColor="#FFFFFF" />
+            <Link className="  text-gray-5 5text-lg  md:text-[16px] lg:text-[15px] " target="_blank" href={{ pathname: "https://line.me/ti/p/NuDh2mAbCf" }} >
                 @phunattakit_supply
             </Link>
-            <Link className="  text-orange text-2xl  md:text-[24px] lg:text-[22px] " target="_blank" href={{ pathname: "tel:+66996785501" }} >
+            <Link className="  text-gray-5 text-2xl  md:text-[24px] lg:text-[22px] " target="_blank" href={{ pathname: "tel:+66996785501" }} >
                 099-678-5501
             </Link>
         </div >
