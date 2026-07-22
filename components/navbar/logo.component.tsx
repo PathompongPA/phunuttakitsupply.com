26d32daf56fad7919f37df9cb022cfd348b53d88
l.pathompong <pathompong.litk@gmail.com>
2026-07-22 13:26:35 +0700

    edit(logo) : adjust size logo in navbar

--- components/navbar/logo.component.tsx
+++ components/navbar/logo.component.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 import logo from "../../public/image/834cfc12-b631-4ea7-891c-6673ef0b647d-removebg-preview.png"
 export default function Logo() {
     return (
-        <Link className="flex justify-center items-center gap-1 p-0 " href={{ pathname: "/" }}>
-            <Image className=" max-w-10 lg:max-w-8.75 " src={logo} alt="" />
+        <Link className="flex justify-center items-center gap-2 p-0 " href={{ pathname: "/" }}>
+            <Image className=" max-w-10 lg:max-w-[45px]  " src={logo} alt="" />
             <div className=" text-nowrap flex flex-col ">
-                <span className="font-bold text-[12px] lg:text-[12px] text-orange ">บริษัท ภูณัฐกิจ ซัพพลายส์ จำกัด</span>
-                <span className=" text-[8px] lg:text-[8px] text-gray-6 font-bold">PHUNATTAKIT SUPPLY COMPANY LIMITED</span>
+                <span className="font-bold text-[14px] lg:text-[16px] text-orange ">บริษัท ภูณัฐกิจ ซัพพลายส์ จำกัด</span>
+                <span className=" text-[10px] lg:text-[12px] text-gray-6 font-bold">PHUNATTAKIT SUPPLY COMPANY LIMITED</span>
             </div>
         </Link>
     )
